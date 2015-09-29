c41e9a40f2db0322468d33760d3093ebd03d1b34
Phillip Shannon & Kelli Margaret <student@epicodus.com>
2015-09-29 15:11:01 -0700

    Add directive that allows hover over button

--- directives/myDirectives.js
+++ directives/myDirectives.js
@@ -1,10 +1,20 @@
 moriartysDen.directive("currentPageCheck", function() {
-
   return function(scope, element, attrs) {
     if (attrs.currentPage == attrs.matchPath || attrs.currentPage == attrs.matchPath2) {
       element.addClass("active");
     } else {
       element.removeClass("active");
     }
+  }
+});
+
+moriartysDen.directive("buttonHover", function() {
+  return function(scope, element) {
+    element.bind("mouseenter", function() {
+      element.toggleClass("hover-button");
+    });
+    element.bind("mouseleave", function() {
+      element.toggleClass("hover-button");
+    });
   }
 });
